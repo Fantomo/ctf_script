c7292065661ae81e3294028b58bf17e09ccd1ad1
fantomo <me_ericzhang@163.com>
2020-07-11 18:37:28 +0800

    凯撒解密

---
+++ crypto/caesar.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+
+import sys
+
+decode_str = "abcdefghijklmnopqrstuvwxyz"
+
+
+def decode_caesar():
+    flag = ''
+    
+    for tmp_char in ciphertext:
+        if tmp_char in ['{', '_', '}']:
+            flag += tmp_char
+        else:
+            decode_index = decode_str.index(tmp_char)
+            index = decode_index + offset
+            if index > 25:
+                index -= 26
+            tmp_char = decode_str[index]
+            flag += tmp_char
+
+    print("flag:", flag)
+
+
+if __name__ == '__main__':
+    try:
+        ciphertext = sys.argv[1]
+        offset = int(sys.argv[2])
+        if offset <= 25:
+        	decode_caesar()
+        else:
+        	print ("Error: offset must be Less than 26")
+    except:
+        print ("Example: python caesar.py ciphertext offset")
